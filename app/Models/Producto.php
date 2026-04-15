2402aa772e9dfe55db3a85ae7a09046a1a9508e6
angela <angelalaurahuaycho@gmail.com>
2026-04-15 15:22:46 -0400

    guardando cambios antes de cambiar de rama

---
+++ app/Models/Producto.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Categoria;
+
+class Producto extends Model
+{
+    protected $fillable = [
+        'codigo',
+        'nombre',
+        'descripcion',
+        'stock',
+        'categoria_id',
+        'imagen'
+    ];
+
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+}
